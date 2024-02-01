a7920a14894eb2fa07082c614acf1b77e980d150
Sania Bansotra <saniabansotra@gmail.com>
2024-02-01 20:31:01 +0530

    message

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 app.use(express.json());
 const USER_MODEL = require("./model/usermodel");
 const { connectDatabase } = require("./connection/connect");
+const cookies = require("cookie-parser");
+app.use(cookies());
 
 app.post("/signup", async (req, res) => {
   try {
@@ -24,6 +26,58 @@ app.post("/signup", async (req, res) => {
     return res.json({ success: true, message: "Signed Up success" });
   } catch (error) {
     return res.json({ success: false, error: error.message });
+  }
+});
+app.post("/login", async (req, res) => {
+  try {
+    let email = req.body.email;
+    let inputPassword = req.body.password;
+    const checkUser = await USER_MODEL.findOne({ email: email });
+    if (!checkUser) {
+      return res.json({
+        success: false,
+        error: "User not found, please signup first",
+      });
+    }
+    let originalPassword = checkUser.password;
+    if (await verifyPassword(inputPassword, originalPassword)) {
+      sendLoginOtp(`+91${checkUser.phonenumber}`);
+      return res.json({ success: true, message: "Please enter OTP to login" });
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, error: "Incorrect Password" });
+    }
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+});
+app.post("/mfaverify", async (req, res) => {
+  try {
+    let email = req.body.email;
+    let inputPassword = req.body.password;
+    const checkUser = await USER_MODEL.findOne({ email: email });
+    if (!checkUser) {
+      return res.json({
+        success: false,
+        error: "User not found, please signup first",
+      });
+    }
+    let originalPassword = checkUser.password;
+    if (
+      (await verifyPassword(inputPassword, originalPassword)) &&
+      (await verifyOtp(`+91${checkUser.phonenumber}`, code))
+    ) {
+      const token = generateToken(checkUser._id);
+      res.cookie("auth_tk", token);
+      return res.json({ success: true, message: "Logged in success" });
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, error: "Incorrect Credentials" });
+    }
+  } catch (error) {
+    return res.json({ success: true, message: error.message });
   }
 });
 
