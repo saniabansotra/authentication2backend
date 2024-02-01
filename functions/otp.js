42418b4c98c4db2f174e58f53cbbc03dc27143b6
Sania Bansotra <saniabansotra@gmail.com>
2024-02-01 22:35:03 +0530

    message

---
+++ functions/otp.js
@@ -0,0 +1,71 @@
+TWILIO_SERVICE_ID = "VAd3b2f78cfd86d1ab7c943cda02a85e9e"; //from the services->verify page
+TWILIO_ACCOUNT_ID = "ACc7ddb9563c9bf1cfb879df898b2ea121"; // account settings
+TWILIO_AUTH_TOKEN = "b428feddea839f1ef7f67b606a33563d"; // account settings below
+
+const client = require("twilio")(TWILIO_ACCOUNT_ID, TWILIO_AUTH_TOKEN);
+
+const sendLoginOtp = (userphone) => {
+  if (!userphone)
+    return {
+      succes: false,
+      error: "Phone Number Missing",
+    };
+
+  client.verify
+    .services(TWILIO_SERVICE_ID)
+    .verifications.create({
+      to: userphone,
+      channel: "sms",
+    })
+    .then((verification) => {
+      return {
+        success: true,
+        status: verification.status,
+      };
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err.code === 60200)
+        return {
+          success: false,
+          error: "Invalid Parameter",
+        };
+      else if (err.code === 60203)
+        return {
+          success: false,
+          error: "Max Send attempts reached",
+        };
+      else if (err.code === 60212)
+        return {
+          success: false,
+          error: "Too many concurrent requests for phone number",
+        };
+      else
+        return {
+          success: false,
+          error: "Server Issue, Try Again Later!",
+        };
+    });
+};
+
+const verifyOtp = async (phonenum, code) => {
+  try {
+    const verification_check = await client.verify
+      .services(TWILIO_SERVICE_ID)
+      .verificationChecks.create({
+        to: phonenum,
+        code: code,
+      });
+    console.log(verification_check.status);
+    if (verification_check.status === "approved") {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+module.exports = { sendLoginOtp, verifyOtp };
--- index.js
+++ index.js
@@ -3,27 +3,33 @@ const app = express();
 app.use(express.json());
 const USER_MODEL = require("./model/usermodel");
 const { connectDatabase } = require("./connection/connect");
+
 const cookies = require("cookie-parser");
 app.use(cookies());
+const verifyToken = require("./tokens/verifyToken");
+const generateToken = require("./tokens/generateToken");
+const { encrytPassword, verifyPassword } = require("./functions/encryption");
+const { sendLoginOtp, verifyOtp } = require("./functions/otp");
 
 app.post("/signup", async (req, res) => {
   try {
-    const checkuser = await USER_MODEL.findOne({
-      email: req.body.email.toLowerCase,
-    });
-    if (checkuser) {
-      return res.json({ success: false, error: "user already registered" });
-    }
-    const user = {
+    const user = new USER_MODEL({
       name: req.body.name,
       email: req.body.name,
-      password: req.body.password,
+      password: await encrytPassword(req.body.password),
       date: req.body.date,
       phonenumber: req.body.phonenumber,
       isUnder18: req.body.isUnder18,
-    };
-    await user.save();
-    return res.json({ success: true, message: "Signed Up success" });
+    });
+    const checkuser = await USER_MODEL.findOne({
+      email: req.body.email.toLowerCase(),
+    });
+    if (!checkuser) {
+      await user.save();
+      return res.json({ success: true, message: "Signed Up success" });
+    }
+
+    return res.json({ success: false, error: "user already registered" });
   } catch (error) {
     return res.json({ success: false, error: error.message });
   }
@@ -52,6 +58,15 @@ app.post("/login", async (req, res) => {
     return res.json({ success: false, message: error.message });
   }
 });
+const checkIfUserLoggedIn = (req, res, next) => {
+  if (verifyToken(req.cookies.auth_tk)) {
+    const userinfo = verifyToken(req.cookies.auth_tk);
+    req.userid = userinfo.id;
+    next();
+  } else {
+    return res.status(401).json({ success: false, error: "UNAUTHORIZED" });
+  }
+};
 app.post("/mfaverify", async (req, res) => {
   try {
     let email = req.body.email;
@@ -78,6 +93,22 @@ app.post("/mfaverify", async (req, res) => {
     }
   } catch (error) {
     return res.json({ success: true, message: error.message });
+  }
+});
+app.get("/currentuser", checkIfUserLoggedIn, async (req, res) => {
+  try {
+    const userid = req.userid;
+    const userdetails = await USER_MODEL.findOne(
+      { _id: userid },
+      { email: 1, name: 1, dob: 1, isUnder18, createdAt: 1 }
+    );
+    if (userdetails) {
+      return res.json({ success: true, data: userdetails });
+    } else {
+      return res.json({ success: false, error: "User Not found" });
+    }
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
   }
 });
 
